80a2c3eb3af6ed9b8d87105bb017dc403b595061
sudhir33 <32663121+sudhir33@users.noreply.github.com>
2020-09-18 20:45:49 +0530

    Add files via upload

--- swap.c
+++ swap.c
@@ -1,12 +1,35 @@
-#include<stdio.h>  
- int main()    
-{    
-int a,b;
-scanf("%d%d",&a,&b);     
-printf(" a is %d and  b is %d \n",a,b);      
-a=a+b;  
-b=a-b;    
-a=a-b;    
-printf(" a is %d and  b is %d",a,b);    
-return 0;  
-}   
+#include<stdio.h>
+int main()
+{
+	int n,e=0XAAAAAAAAAAAAAAAA,o=0x5555555555555555;
+	scanf("%d",&n);
+	e=n&e;
+	o=n&o;
+	e=e>>1;
+	o=o<<1;
+	printf("%d",e|o);
+	
+}
+
+/*
+n=200
+n=1100 1000      
+
+e=0000 0100 0100>>1
+      or
+o=0000 1000 0000<<1
+-------------------
+  0000 1100 0100
+*/
+
+
+
+
+
+
+
+
+
+
+
+
